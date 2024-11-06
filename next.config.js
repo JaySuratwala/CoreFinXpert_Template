9aead70f022ebfe327d01180e14536137f66a7e4
Jay Suratwala <81503183+JaySuratwala@users.noreply.github.com>
2024-11-06 10:18:50 +0000

    Prepare for deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    output: 'export',
+    basePath: '/CFX_template',  // This matches your repository name
+    trailingSlash: true,        // Ensures proper path handling on GitHub Pages
+    images: {
+        domains: ['aceternity.com','assets.aceternity.com','w3.org','image.flaticon.com'] // Replace with your actual hostnames
+    },
+}
