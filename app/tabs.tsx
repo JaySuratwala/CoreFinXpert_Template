d8f9bf3a03d5e8c1c1d173ce9b2d1389b1391c29
Jay Suratwala <81503183+JaySuratwala@users.noreply.github.com>
2024-11-07 21:24:09 +0000

    Fix type compatibility for image property in socialHandles

--- app/tabs.tsx
+++ app/tabs.tsx
@@ -63,7 +63,7 @@ export function TabsDemo() {
     ];
     return (
         <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-40 gap-10">
-            <h2 className="font-bold text-5xl text-custom-purple">DhanXpert</h2>
+            <h2 className="font-bold text-5xl text-custom-purple">Project</h2>
             <Tabs tabs={tabs} />
         </div>
     );
