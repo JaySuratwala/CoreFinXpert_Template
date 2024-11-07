d1bf03786be9ede763e29fa42d326d2680e22f79
Jay Suratwala <81503183+JaySuratwala@users.noreply.github.com>
2024-11-07 21:51:47 +0000

    Fixed image property type in socialHandles

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     output: 'export',
-    basePath: '/CFX_template',  // This matches your repository name
-    trailingSlash: true,        // Ensures proper path handling on GitHub Pages
+    basePath: process.env.NODE_ENV === 'production' ? '/CFX_template' : '' ,
+    trailingSlash: true,
+    reactStrictMode: true,
     images: {
-        domains: ['aceternity.com','assets.aceternity.com','w3.org','image.flaticon.com'] // Replace with your actual hostnames
+        domains: ['aceternity.com', 'assets.aceternity.com', 'w3.org', 'image.flaticon.com'],
+        unoptimized: true,
     },
-}
+};
